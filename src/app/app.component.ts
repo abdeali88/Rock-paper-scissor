637d3457d913948eaa43140aa0da1e9d8b84c1ba
Abdeali Arsiwala <abdeali.a@somaiya.edu>
2019-11-12 17:26:35 +0530

    my-project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,89 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rps-game';
+  disableWeapon = "";
+  statusColor = "";
+  userScore = 0;
+  compScore = 0;
+  userSelected: string;
+  compSelected: string;
+  action: string;
+  status: string;
+  compWeapons = [
+    'rock',
+    'paper',
+    'scissors'
+  ];
+
+  userPick(userWeapon: string): void {
+    this.disableWeapon = "disable";
+    this.userSelected = userWeapon;
+    console.log( this.userSelected);
+    setTimeout( () => {
+      const randomNum = Math.floor(Math.random() * 3);
+      this.compSelected = this.compWeapons[randomNum];
+      console.log(this.compSelected);
+      this.checkResult();
+    }, 100);
+  }
+
+  clearField() {
+    setTimeout(() => {
+      this.disableWeapon = '';
+      this.status = '';
+      this.userSelected = '';
+      this.compSelected = '';
+    }, 3000);
+  }
+
+  win(user, comp) {
+    this.statusColor="#03fc0f";
+    this.userScore ++;
+    this.userSelected = user;
+    this.compSelected = comp;
+    this.action = 'beats';
+    this.status = '. You win!';
+    this.clearField();
+  }
+
+
+  lose(user, comp) {
+    this.statusColor="#fc4103";
+    this.compScore ++;
+    this.userSelected = user;
+    this.compSelected = comp;
+    this.action = 'loses to';
+    this.status = '. You lose!';
+    this.clearField();
+  }
+
+  draw(user, comp) {
+    this.statusColor="#ffffff";
+    this.userSelected = user;
+    this.compSelected = comp;
+    this.action = 'and';
+    this.status = '. Its a Draw!';
+    this.clearField();
+  }
+
+  checkResult() {
+    const userChoice = this.userSelected;
+    const compChoice = this.compSelected;
+    switch (userChoice + compChoice) {
+      case 'rockscissors':
+      case 'paperrock':
+      case 'scissorspaper':
+        this.win(userChoice, compChoice);
+        break;
+      case 'rockpaper':
+      case 'scissorsrock':
+      case 'paperscissors':
+        this.lose(userChoice, compChoice);
+        break;
+      default:
+        this.draw(userChoice, compChoice);
+        break;
+    }
+
+  }
 }
